3b2d5e0573baaef0332883ff0703af7d0b80bfb4
Veronika <Veronika@DESKTOP-S01858F>
2020-04-03 15:25:15 +0300

    Add project files.

---
+++ ProductShop/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if (Session.Count == 0)
+            {
+                Session["Products"] = new List<Product>();
+            }
+            var products = Session["Products"] as List<Product>;
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct( Product product)
+        {
+            return View();
+        }
+    }
+}
---
+++ ProductShop/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductShop.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ProductionDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ExcpiryDate { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        Meausure Meausure { get; set; }
+        public string Logo { get; set; }
+
+    }
+}
